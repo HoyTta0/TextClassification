b11f18dd4656377f00a648fa68620112565daf4f
HoyTra0 <893422529@qq.com>
2019-11-11 15:51:00 +0800

    新增预测功能

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ Created on 2019/11/5 1:50 PM
 
 from model import tfidf
 from data_proc import load_data
+from predict import predictText
 if __name__ == '__main__':
     # 加载数据
     X_train, X_test, y_train, y_test = load_data()
     # 模型测试
-    tfidf_clf=tfidf(X_train,X_test,y_train,y_test)
+    # tfidf_clf=tfidf(X_train,X_test,y_train,y_test)
+    #模型预测
+    predict=predictText(X_train)
