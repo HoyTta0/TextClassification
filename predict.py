d52cb3de1de8431be1fb4dcf8bc89bc64b11369e
HoyTra0 <893422529@qq.com>
2019-11-11 15:50:33 +0800

    预测数据并导出为csv文件

--- predict.py
+++ predict.py
@@ -9,44 +9,43 @@ import pickle
 import jieba
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
+def predictText(X_train):
 
-def predictText(text):
+    data = pd.read_csv('before.csv', encoding="utf-8", header=None)
+    data.columns = ['n_id', 'id', 'content', 'label']
+    data = data.drop(index=[0])
 
-    #文本预处理
-    comp = re.compile('[^\u4e00-\u9fa5]')
-    text = comp.sub('', text)
     lines = open('stopwords-master/百度停用词表.txt', 'r', encoding='utf-8')
     stop_words = [line.strip() for line in lines]
     # print(stop_words)
     word_list = []
     words_list = []
-    try:
-        words = jieba.cut(text)
-        words = [word for word in words if word not in stop_words]
-        segmented_words = ','.join(words)
-    except AttributeError:
-        pass
-    finally:
-        words_list.append(words)
-        word_list.append(segmented_words.strip())
-    text = word_list
-    print(text)
-    #预测
-    train_path = "train.pkl"
-    with open(train_path, 'rb') as out_data:
-        # 按保存变量的顺序加载变量
-        X_train = pickle.load(out_data)
-    tfidf_vectorizer = TfidfVectorizer()
-    train = tfidf_vectorizer.fit_transform(X_train)
-    text_test = tfidf_vectorizer.transform(text)
+    for sent in data['content']:
+        try:
+            words = jieba.cut(sent)
+            words = [word for word in words if word not in stop_words]
+            segmented_words = ','.join(words)
+        except AttributeError:
+            continue
+        finally:
+            words_list.append(words)
+            word_list.append(segmented_words.strip())
+    data['tokens'] = word_list
+
+    def tfidf(data):
+        tfidf_vectorizer = TfidfVectorizer()
+        train = tfidf_vectorizer.fit_transform(data)
+        return train, tfidf_vectorizer
+    # 文本特征提取
+    X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
+    X_test_tfidf = tfidf_vectorizer.transform(data['tokens'])
+
     tfidf_path = 'tfidf.pkl'
     with open(tfidf_path, 'rb') as out_data:
         clf_tfidf = pickle.load(out_data)
-    text_predicted = clf_tfidf.predict(text_test)
-    print(text_predicted)
+    y_predicted_tfidf = clf_tfidf.predict(X_test_tfidf)
+    data['label'] = y_predicted_tfidf
 
-
-# if __name__ == '__main__':
-#     text=input()
-#     predictText(text)
+    data.to_csv('after.csv', encoding="utf_8_sig",columns=[ 'id', 'content', 'label'])
